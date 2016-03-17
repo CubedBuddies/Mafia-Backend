0488774226da4ac287d88ed401ab270f04e2ef34
Rick Song <ricksong@squareup.com>
2016-03-16 21:17:55 -0700

    Adds default image

--- app/models/player.rb
+++ app/models/player.rb
@@ -20,7 +20,7 @@ class Player < ActiveRecord::Base
 
   has_attached_file :avatar,
                     styles: { medium: "300x300>", thumb: "100x100>" },
-                    default_url: "/images/:style/missing.png"
+                    default_url: "/images/default_player.png"
 
   belongs_to :game
 
